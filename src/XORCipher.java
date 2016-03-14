8dc981ce795dcc7c1c55884f37ce42a1a75a86e0
ErenTl <beneyim@gmail.com>
2016-03-15 01:28:11 +0200

    Added files via upload

---
+++ src/XORCipher.java
@@ -0,0 +1,21 @@
+public class XORCipher {
+	
+	private static String cryption(String message,String title) {
+		StringBuilder output = new StringBuilder();
+		int lenght = Math.max(message.length(), title.length());
+		
+		for (int i = 0; i < lenght; i++) {
+			output.append(((char) (message.charAt(i % message.length()) ^ title.charAt(i % title.length())))+0);
+			output.append(", ");
+		}
+		System.out.println(output);
+
+		return "";
+	}
+
+	public static void main(String[] args) {
+		cryption("This is passawords.", "passawords");
+		
+	}
+
+}
