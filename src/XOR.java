6b238b625a59e4dc793dc005630b338cc885fd9f
Eren Torun <beneyim@gmail.com>
2016-03-15 03:30:18 +0200

    Update and rename XOREncryption.java to XOR.java

---
+++ src/XOR.java
@@ -0,0 +1,23 @@
+public class XOR {
+
+	public static void main(String[] args) {
+		String password="";//password
+		char message[] = {};
+		char[] key = password.toCharArray();//key
+		
+		int l = message.length;
+		int l1 = key.length;
+		
+		
+		for(int i=0;i<l;i++) {
+			message[i] = (char) (message[i] ^ key[i% l1]);
+		}
+		
+		System.out.println(message);
+	
+	
+	}
+	
+	
+
+}
